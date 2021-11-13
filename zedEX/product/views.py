e458ccc2910fa9e253cfa356a22fbc994b1469be
Aifedayo <akeemifedayolag@gmail.com>
2021-11-13 18:12:31 +0100

    BE views

--- zedEX/product/views.py
+++ zedEX/product/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Product
+from .serializers import ProductSerializer
+
