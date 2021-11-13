c7f02c9d2a2a76a8087c45f44c2ad9700eb415a5
Aifedayo <akeemifedayolag@gmail.com>
2021-11-13 18:14:56 +0100

    BE views

--- zedEX/product/views.py
+++ zedEX/product/views.py
@@ -6,3 +6,7 @@ from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 
+class LatestProductsList(APIView):
+    def get(self, request, format=None):
+        products = Product.objects.all()[:5]
+        serializer = ProductSerializer(products, many=True)
