df8093d51546ce36b77a0d4ce14e63ce08ce41e8
Aifedayo <akeemifedayolag@gmail.com>
2021-11-13 18:05:28 +0100

    BE serializers

--- zedEX/product/serializers.py
+++ zedEX/product/serializers.py
@@ -5,4 +5,8 @@ from .models import Category, Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = (
+            'id', 'name', 'get_absolute_url',
+            'description', 'price', 'get_image',
+            'get_thumbnail'
+        )
