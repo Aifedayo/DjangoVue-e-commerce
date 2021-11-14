b6cc2cb08a485e3fee44b628f17ee7b3830e270a
Aifedayo <akeemifedayolag@gmail.com>
2021-11-14 03:25:14 +0100

    FE product view

--- zedex_vue/src/views/Category.vue
+++ zedex_vue/src/views/Category.vue
@@ -31,6 +31,28 @@ export default {
             const categorySlug = this.$route.params.category_slug
 
             this.$store.commit('setIsLoading', true)
+
+            axios
+                .get('/api/v1/products/${categorySlug}/')
+                then(response => {
+                    this.category = response.data
+
+                    document.title = this.category.name + ' | zedEx'
+                })
+                .catch(error => {
+                    console.log(error)
+
+                    toast({
+                        message: "Something went wrong. Please try again",
+                        type: 'is-danger',
+                        dismissible: true,
+                        pauseOnHover: true,
+                        duration: 2000,
+                        position: 'bottom-right',
+                    })
+                })
+            
+            this.$store.commit('setIsLoading', false)
         }
     }
 }
