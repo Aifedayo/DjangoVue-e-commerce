9079563b2b5c006dc889d818e3c8440ac4e508fe
Aifedayo <akeemifedayolag@gmail.com>
2021-11-15 21:29:30 +0100

    FE product view

--- zedex_vue/src/components/CartItem.vue
+++ zedex_vue/src/components/CartItem.vue
@@ -8,7 +8,7 @@
             <a @click="incrementQuantity(item)"> + </a>
         </td>
         <td>${{ getItemTotal(item).toFixed(2) }}</td>
-        <td><button class="delete"></button></td>
+        <td><button class="delete" @click="removeFromCart(item)"></button></td>
     </tr>
 </template>
 
@@ -44,7 +44,11 @@ export default {
         updateCart() {
             localStorage.setItem('cart', JSON.stringify(this.$store.state.cart))
         },
-        
+        removeFromCart(item) {
+            this.$emit('removeFromCart', item)
+
+            this.updateCart()
+        },
     }
 }
 </script>
