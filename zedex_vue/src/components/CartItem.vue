7f64bca9005a3699a521e55f42a49ed11c44f605
Aifedayo <akeemifedayolag@gmail.com>
2021-11-15 21:20:53 +0100

    FE product view

--- zedex_vue/src/components/CartItem.vue
+++ zedex_vue/src/components/CartItem.vue
@@ -3,7 +3,9 @@
         <td><router-link :to="item.product.get_absolute_url">{{ item.product.name }}</router-link></td>
         <td>${{ item.product.price }}</td>
         <td>
+            <a @click="decrementQuantity(item)"> - </a>
             {{ item.quantity }}
+            <a @click="incrementQuantity(item)"> + </a>
         </td>
         <td>${{ getItemTotal(item).toFixed(2) }}</td>
         <td><button class="delete"></button></td>
@@ -25,6 +27,14 @@ export default {
     methods: {
         getItemTotal(item) {
             return item.quantity * item.product.price
+        },
+        decrementQuantity(item) {
+            item.quantity -= 1
+            this.updateCart()
+        },
+        incrementQuantity(item) {
+            item.quantity += 1
+            this.updateCart()
         }
     }
 }
