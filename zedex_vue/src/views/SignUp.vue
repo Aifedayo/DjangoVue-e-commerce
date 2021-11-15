69ef611fe5caf1f9d48cd2f3df8b4521a4c5cc9b
Aifedayo <akeemifedayolag@gmail.com>
2021-11-15 21:35:40 +0100

    FE product view

---
+++ zedex_vue/src/views/SignUp.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="page-sign-up">
+
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { toast } from 'bulma-toast'
+
+export default {
+    name: 'SignUp'
+}
+</script>
+
+<style>
+
+</style>
