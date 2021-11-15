293f80f6fe8c737f7b6c177c5ec8345d0e924852
Aifedayo <akeemifedayolag@gmail.com>
2021-11-15 20:52:45 +0100

    FE product view

--- zedex_vue/src/views/Cart.vue
+++ zedex_vue/src/views/Cart.vue
@@ -4,13 +4,49 @@
             <div class="column is-12">
                 <h1 class="title">Cart</h1>
             </div>
+
+            <div class="column is-12 box">
+                <table class="table is-fullwidth" v-if="cartTotalLength">
+                    <thead>
+                        <tr>
+                            <th>Product</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Total</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <CartItem
+                            v-for="item in cart.items"
+                            v-bind:key="item.product.id"
+                            v-bind:initialItem="item" 
+                        />
+                    </tbody>
+                </table>
+                <p v-else>You do not have any products in your cart ... </p>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+import CartItem from '@/components/CartItem.vue'
+
 export default {
-    name: 'Cart'
+    name: 'Cart',
+    components: {
+        CartItem
+    },
+    
+    data() {
+        return {
+            cart: {
+                items: []
+            }
+        }
+    }
 }
 </script>
 
