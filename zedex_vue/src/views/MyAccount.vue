655de50be3c81db34b004935ccc4cb7fc289a0df
Aifedayo <akeemifedayolag@gmail.com>
2021-11-17 18:13:17 +0100

    FE product view

--- zedex_vue/src/views/MyAccount.vue
+++ zedex_vue/src/views/MyAccount.vue
@@ -4,7 +4,21 @@
 
 <script>
 export default {
+    name: 'MyAccount',
 
+    methods: {
+        logout() {
+            axios.defaults.headers.common['Authorization'] = ""
+
+            localStorage.removeItem("token")
+            localStorage.removeItem("username")
+            localStorage.removeItem("userid")
+            
+            this.$store.commit('removeToken')
+
+            this.$routr.push('/')
+        }
+    }
 }
 </script>
 
