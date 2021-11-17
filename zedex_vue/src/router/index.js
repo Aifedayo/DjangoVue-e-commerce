72816491be3b98f59145b2b0bc8b9c05ac8a23ae
Aifedayo <akeemifedayolag@gmail.com>
2021-11-17 18:07:46 +0100

    FE product view

--- zedex_vue/src/router/index.js
+++ zedex_vue/src/router/index.js
@@ -5,6 +5,8 @@ import Product from '../views/Product.vue'
 import Category from '../views/Category.vue'
 import Search from '../views/Search.vue'
 import Cart from '../views/Cart.vue'
+import SignUp from '../views/SignUp.vue'
+import Login from '../views/Login.vue'
 
 const routes = [
   {
@@ -39,6 +41,16 @@ const routes = [
     path: '/cart/',
     name: 'Cart',
     component: Cart
+  },
+  {
+    path: '/sign-up/',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/log-in/',
+    name: 'Login',
+    component: Login
   }
 ]
 
