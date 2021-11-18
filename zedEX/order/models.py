79fd60de0e229fa26ec29d04caa8bb8d0d131fa0
Aifedayo <akeemifedayolag@gmail.com>
2021-11-18 21:38:37 +0100

    FE product view

--- zedEX/order/models.py
+++ zedEX/order/models.py
@@ -2,3 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 from product.models import Product
+
+class Order(models.Model):
+    user = models.ForeignKey(User, related_name='orders', on_delete=models.CASCADE)
