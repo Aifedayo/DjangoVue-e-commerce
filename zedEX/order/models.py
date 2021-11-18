0fd2eaddcf780185c6edbf2b0035a2c12add3efa
Aifedayo <akeemifedayolag@gmail.com>
2021-11-18 21:39:51 +0100

    FE product view

--- zedEX/order/models.py
+++ zedEX/order/models.py
@@ -7,3 +7,4 @@ class Order(models.Model):
     user = models.ForeignKey(User, related_name='orders', on_delete=models.CASCADE)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
