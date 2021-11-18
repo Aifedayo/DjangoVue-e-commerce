0a2b7e6f56553a0e6fbb3564efa12f03b7f1a7c1
Aifedayo <akeemifedayolag@gmail.com>
2021-11-18 21:44:35 +0100

    BE order

--- zedEX/order/models.py
+++ zedEX/order/models.py
@@ -14,3 +14,9 @@ class Order(models.Model):
     phone = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     paid_amount = models.DecimalField(max_digits=12, decimal_places=2, blank=True, null=True)
+    stripe_token = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    
