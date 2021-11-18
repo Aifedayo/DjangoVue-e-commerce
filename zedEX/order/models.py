982ca559879a19fe29ef53a1be09b32551e793bf
Aifedayo <akeemifedayolag@gmail.com>
2021-11-18 22:09:07 +0100

    BE order

--- zedEX/order/models.py
+++ zedEX/order/models.py
@@ -27,3 +27,7 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, related_name='items', on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=12, decimal_places=2)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return '%s' % self.id
