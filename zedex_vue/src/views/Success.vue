f6def9f05d7967a011adf9a4f678b4ade077356b
Aifedayo <akeemifedayolag@gmail.com>
2021-11-18 21:26:42 +0100

    FE product view

---
+++ zedex_vue/src/views/Success.vue
@@ -0,0 +1,17 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name: 'Success',
+
+    mounted() {
+        document.title = 'Success | zedEX'
+    }
+}
+</script>
+
+<style>
+
+</style>
