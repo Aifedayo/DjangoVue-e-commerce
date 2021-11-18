e3c6129a0d12dbd8ea4047ff6fcd7179a2b1f782
Aifedayo <akeemifedayolag@gmail.com>
2021-11-18 21:40:14 +0100

    FE product view

--- zedEX/order/models.py
+++ zedEX/order/models.py
@@ -8,3 +8,4 @@ class Order(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
