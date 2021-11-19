0ec15209845e3f02f14e5f329cebc0b1f465fd11
Aifedayo <akeemifedayolag@gmail.com>
2021-11-19 16:33:47 +0100

    FE Checkout

--- zedEX/order/views.py
+++ zedEX/order/views.py
@@ -15,7 +15,7 @@ from .serializers import OrderSerializer
 
 # Create your views here.
 @api_view(['POST'])
-@authentication_classes([authentication.TokenAuthenticated])
+@authentication_classes([authentication.TokenAuthentication])
 @permission_classes([permissions.IsAuthenticated])
 def checkout(request):
     serializer = OrderSerializer(data=request.data)
