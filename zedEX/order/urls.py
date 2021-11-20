ddb87e82d9fec8bb2c6261b8081592ba0eb49939
Aifedayo <akeemifedayolag@gmail.com>
2021-11-20 09:12:59 +0100

    BE serializer

--- zedEX/order/urls.py
+++ zedEX/order/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from order import views
 
 urlpatterns = [
-    path('checkout/', views.checkout)
+    path('checkout/', views.checkout),
+    path('orders/', views.OrdersList.as_view()),
 ]
 
 accounts_urlpatterns = [
