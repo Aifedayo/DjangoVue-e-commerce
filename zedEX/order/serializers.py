2279012e1e343ac357598d68c8f3b4056c3cfd8e
Aifedayo <akeemifedayolag@gmail.com>
2021-11-20 09:02:15 +0100

    FE Checkout

--- zedEX/order/serializers.py
+++ zedEX/order/serializers.py
@@ -42,6 +42,24 @@ class OrderSerializer(serializers.ModelSerializer):
             "items",
         )
 
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True)
+    class Meta:
+        model = Order
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+            "email",
+            "address",
+            "zipcode",
+            "place",
+            "phone",
+            "stripe_token",
+            "items",
+        )
+
     def create(self, validated_data):
         items_data = validated_data.pop('items')
         order = Order.objects.create(**validated_data)
